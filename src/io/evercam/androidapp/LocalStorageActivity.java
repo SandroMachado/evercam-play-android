5fd4614d686c16be97f4dd962db5e6a545353794
Liuting Du <kangtadlt@gmail.com>
2014-10-01 13:32:28 +0100

    better local storage

--- src/io/evercam/androidapp/LocalStorageActivity.java
+++ src/io/evercam/androidapp/LocalStorageActivity.java
@@ -2,6 +2,8 @@ package io.evercam.androidapp;
 
 import com.bugsense.trace.BugSenseHandler;
 import com.hikvision.netsdk.NET_DVR_TIME;
+
+import io.evercam.androidapp.custom.CustomProgressDialog;
 import io.evercam.androidapp.dto.EvercamCamera;
 import io.evercam.androidapp.utils.Constants;
 import io.evercam.androidapp.video.VideoActivity;
@@ -14,6 +16,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.SurfaceView;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
@@ -26,6 +29,9 @@ public class LocalStorageActivity extends Activity
 	private EvercamCamera evercamCamera;
 	private SurfaceView surfaceView;
 	private HikvisionSdk hikvisionSdk;
+	
+	private CustomProgressDialog customProgressDialog;
+	Handler handler = new Handler();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -39,14 +45,62 @@ public class LocalStorageActivity extends Activity
 		}
 
 		EvercamPlayApplication.sendScreenAnalytics(this, getString(R.string.screen_local_storage));
+		
+		if (this.getActionBar() != null)
+		{
+			this.getActionBar().setHomeButtonEnabled(true);
+		}
 
 		surfaceView = (SurfaceView) findViewById(R.id.surface_hikvision);
+		
+		int screenWidth = CamerasActivity.readScreenWidth(this);
+		int screenHeight = CamerasActivity.readScreenHeight(this);
+		if(screenWidth < screenHeight)
+		{
+			android.widget.LinearLayout.LayoutParams params = new android.widget.LinearLayout.LayoutParams(screenWidth, screenWidth/3 * 2);
+			surfaceView.setLayoutParams(params);
+		}
 
 		evercamCamera = VideoActivity.evercamCamera;
 
 		hikvisionSdk = new HikvisionSdk(surfaceView, evercamCamera);
+		
+		customProgressDialog = new CustomProgressDialog(this);
+		customProgressDialog.show("Connecting camera...");
+		
+		handler.postDelayed(new Runnable(){
 
-		loginToDevice();
+			@Override
+			public void run()
+			{
+				loginToDevice();
+			}
+		}, 500);
+	}
+	
+	@Override
+	public void onStart()
+	{
+		super.onStart();
+
+		if (Constants.isAppTrackingEnabled)
+		{
+			BugSenseHandler.startSession(this);
+		}
+	}
+
+	@Override
+	public void onStop()
+	{
+		super.onStop();
+		
+		finish();
+		hikvisionSdk.cleanUp();
+		
+		if (Constants.isAppTrackingEnabled)
+		{
+			BugSenseHandler.closeSession(this);
+		}
 	}
 
 	@Override
@@ -77,7 +131,6 @@ public class LocalStorageActivity extends Activity
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item)
 	{
-
 		switch (item.getItemId())
 		{
 		case R.id.menu_start_datetime:
@@ -95,9 +148,9 @@ public class LocalStorageActivity extends Activity
 	private void loginToDevice()
 	{
 		boolean loginSuccess = hikvisionSdk.login();
+		customProgressDialog.dismiss();
 		if (loginSuccess)
 		{
-
 			showDateTimePickerDialog();
 		}
 	}
@@ -119,46 +172,15 @@ public class LocalStorageActivity extends Activity
 					@Override
 					public void onClick(DialogInterface dialog, int which)
 					{
-						int month = datePicker.getMonth();
-						int day = datePicker.getDayOfMonth();
-						int year = datePicker.getYear();
-
-						int hour = timePicker.getCurrentHour();
-						int min = timePicker.getCurrentMinute();
-
-						String strMin = String.valueOf(min);
-						String strHour = String.valueOf(hour);
-
-						if (strMin.length() == 1)
-						{
-							strMin = "0" + strMin;
-						}
-
-						if (strHour.length() == 1)
-						{
-							strHour = "0" + strHour;
-						}
-
-						String date = month + "/" + day + "/" + year + " ";
-						String time = strHour + ":" + strMin + " ";
-
-						Log.d(TAG, "From: " + date + time);
-						NET_DVR_TIME beginTime = new NET_DVR_TIME();
-						// beginTime.dwYear = 2014;
-						// beginTime.dwMonth = 9;
-						// beginTime.dwDay = 29;
-						// beginTime.dwHour = 0;
-						// beginTime.dwMinute = 0;
-						// beginTime.dwSecond = 0;
-						beginTime.dwYear = year;
-						beginTime.dwMonth = month;
-						beginTime.dwDay = day;
-						beginTime.dwHour = hour;
-						beginTime.dwMinute = min;
-						beginTime.dwSecond = 0;
-
 						surfaceView.setVisibility(View.VISIBLE);
-						hikvisionSdk.startPlayback(beginTime);
+						handler.postDelayed(new Runnable(){
+
+							@Override
+							public void run()
+							{
+								hikvisionSdk.startPlayback(getTimeFromPicker(datePicker, timePicker));
+							}
+						}, 500);
 					}
 				}).setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener(){
 
@@ -170,5 +192,38 @@ public class LocalStorageActivity extends Activity
 				});
 
 		dialogBuilder.create().show();
+	}
+	
+	private NET_DVR_TIME getTimeFromPicker(DatePicker datePicker, TimePicker timePicker)
+	{
+		int month = datePicker.getMonth() + 1;
+		int day = datePicker.getDayOfMonth();
+		int year = datePicker.getYear();
+
+		int hour = timePicker.getCurrentHour();
+		int min = timePicker.getCurrentMinute();
+
+		String strMin = String.valueOf(min);
+		String strHour = String.valueOf(hour);
+
+		if (strMin.length() == 1)
+		{
+			strMin = "0" + strMin;
+		}
+
+		if (strHour.length() == 1)
+		{
+			strHour = "0" + strHour;
+		}
+
+		NET_DVR_TIME beginTime = new NET_DVR_TIME();
+		beginTime.dwYear = year;
+		beginTime.dwMonth = month;
+		beginTime.dwDay = day;
+		beginTime.dwHour = hour;
+		beginTime.dwMinute = min;
+		beginTime.dwSecond = 0;
+		
+		return beginTime;
 	}
 }
