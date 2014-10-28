fc70af617518ea0991ad5f6bb448ea24f95b08ee
Liuting Du <kangtadlt@gmail.com>
2014-10-28 11:37:37 +0000

    boundry if no logo recieved

--- src/io/evercam/androidapp/ScanActivity.java
+++ src/io/evercam/androidapp/ScanActivity.java
@@ -23,6 +23,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -345,7 +346,17 @@ public class ScanActivity extends Activity
 		@Override
 		protected void onPostExecute(Drawable drawable)
 		{
-			drawableArray.put(position, drawable);
+			drawable = null;
+			if(drawable != null)
+			{
+				drawableArray.put(position, drawable);
+			}
+			else
+			{
+				Drawable questionImage = ScanActivity.this.getResources().getDrawable( R.drawable.question_img_trans);
+				drawableArray.put(position, questionImage);
+			}
+
 			deviceAdapter.notifyDataSetChanged();
 		}
 	}
